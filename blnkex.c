023c0d65abef81cb782991351293c13e36dce6f3
harshasingh <harshasingh296@gmail.com>
2016-10-12 23:39:12 +0530

    Create blnkex.c
    copy its input to its output, replacing each string of one or more blanks by a single blank.

---
+++ blnkex.c
@@ -0,0 +1,1 @@
+int main(void){  int ch;  int blank;  blank = 0;  while((ch = getchar()) != EOF)  {    if(c == ' ')    {      if(blank == 0)        /*one blank encountered*/      {        blank = 1;        putchar(c);      }    }       if(c != ' ')    {      blank = 0;      putchar(c);    }  }  return 0;}
