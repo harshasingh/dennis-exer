3c86c9a4123109abf8980662737d2b98fbf4c85e
harshasingh <harshasingh296@gmail.com>
2016-10-13 00:12:26 +0530

    Update blnkex.c

--- blnkex.c
+++ blnkex.c
@@ -1,1 +1,19 @@
-int main(void){  int ch;  int blank;  blank = 0;  while((ch = getchar()) != EOF)  {    if(c == ' ')    {      if(blank == 0)        /*one blank encountered*/      {        blank = 1;        putchar(c);      }    }       if(c != ' ')    {      blank = 0;      putchar(c);    }  }  return 0;}
+#include<stdio.h>
+int main(void){ 
+    int ch; 
+    int blank; 
+    blank = 0;  
+    while((ch = getchar()) != EOF)  {  
+        if(c == ' ')    {      
+            if(blank == 0)        /*one blank encountered*/     
+            {        
+                blank = 1;       
+                putchar(c);     
+            }  
+        }     
+        if(c != ' ')    {     
+            blank = 0;     
+            putchar(c);    
+        }  } 
+    return 0;}
+}
